66880fd870713fd1f0e7550aaf31af2533385a88
florinen <nenfl24@gmail.com>
2019-04-25 18:18:21 -0500

    misspelling

--- route_table_assoc.tf
+++ route_table_assoc.tf
@@ -1,5 +1,5 @@
 # Associate subnet pub_2_subnet_eu_west_1a to public route table
-resource "aws_route_tale_association" "pub_1_subnet_eu_west_1a_assoc" {
+resource "aws_route_table_association" "pub_1_subnet_eu_west_1a_assoc" {
     subnet_id = "${aws_subnet.pub_1_subnet_eu_west_1a.id}"
     route_table_id = "${aws_vpc.vpc_test.main_route_table_id}"
   
