2a374d83c49e28d915427d944c2ed50b60417368
florinen <nenfl24@gmail.com>
2019-04-25 18:01:56 -0500

    removed curly brackets

--- eip.tf
+++ eip.tf
@@ -1,6 +1,6 @@
 resource "aws_eip" "test_eip" {
     vpc = "true"
-    depends_on = ["${aws_internet_gateway.igw}"]
+    depends_on = ["aws_internet_gateway.igw"]
 
     tags = {
         Name = "test_eip"
