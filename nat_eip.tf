a5772c36c2fef3341e06f0445e707acc46d45d29
florinen <nenfl24@gmail.com>
2019-04-25 14:28:46 -0500

    updated file

--- nat_eip.tf
+++ nat_eip.tf
@@ -1,5 +1,5 @@
 resource "aws_eip" "nat-eip" {
-    subnet_id = "${var.private_subnet_cidr.id}"
+    subnet_id = "${var.public_subnet_cidr.id}"
     depends_on = ["aws_internet_gateway.test-IGW"]
     tags = {
         Name = "nat-eip"
