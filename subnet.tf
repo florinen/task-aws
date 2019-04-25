c156af15dd81daeacd41814350bf014aa7aeb55c
florinen <nenfl24@gmail.com>
2019-04-25 12:40:46 -0500

    subnet file created

--- subnet.tf
+++ subnet.tf
@@ -7,3 +7,12 @@ resource "aws_subnet" "test_1" {
     }
   
 }
+resource "aws_subnet" "test_2" {
+    vpc_id = "${aws_vpc.test.id}"
+    cidr_block = "192.168.2.0/24"
+
+    tags = {
+        Name = "test_2"    
+    }
+  
+}
