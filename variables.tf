2d60db01b9a5b9f827ea5145cb4a22ede41330da
florinen <florinen>
2019-04-26 16:05:42 -0500

    fixed ami var

--- variables.tf
+++ variables.tf
@@ -5,9 +5,7 @@ variable "aws_region" {
 
 variable "amis" {
     description = "AMIs by region"
-    default = {
-        eu-west-1 = "ami-0ff760d16d9497662" # Centos 7 x64
-    }
+    default "ami-0ff760d16d9497662" # Centos 7 x64
 }
 
 variable "vpc_cidr" {
