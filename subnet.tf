c667fbbd0468f5138a57c525bf9b48a345ca3be2
florinen <nenfl24@gmail.com>
2019-04-26 10:26:42 -0500

    reverted back to one subnet

--- subnet.tf
+++ subnet.tf
@@ -2,18 +2,12 @@ resource "aws_subnet" "pub_1_subnet_eu_west_1a" {
     vpc_id = "${aws_vpc.vpc_test.id}"
     cidr_block = "${var.pub_1_subnet_cidr}"
     map_public_ip_on_launch = true  #any instance created in this subnet will have a public IP
-    availability_zone = ["eu-west-1a","eu-west-1b,eu-west-1c"]
+    availability_zone = "eu-west-1a"
     tags = {
         Name = "pub_test_1"    
     }
-resource "aws_subnet" "pub_2_subnet_eu_west_1b" {
-    vpc_id = "${aws_vpc.vpc_test.id}"
-    cidr_block = "${var.pub_2_subnet_cidr}"
-    map_public_ip_on_launch = true  #any instance created in this subnet will have a public IP
-    availability_zone = "eu-west-1b"
-    tags = {
-        Name = "pub_test_2"    
-    }
+    
+  
 }
 resource "aws_subnet" "priv_1_subnet_eu_west_1a" {
     vpc_id = "${aws_vpc.vpc_test.id}"
@@ -22,13 +16,5 @@ resource "aws_subnet" "priv_1_subnet_eu_west_1a" {
     tags = {
         Name = "priv_test_1"    
     }
-resource "aws_subnet" "priv_2_subnet_eu_west_1b" {
-    vpc_id = "${aws_vpc.vpc_test.id}"
-    cidr_block = "${var.priv_2_subnet_cidr}"
-    availability_zone = "eu-west-1b"
-    tags = {
-        Name = "priv_test_2"    
-    }
-   }
-  }
+  
 }
