e829891a6f6888a54d4fc2826a3521a352760bdf
florinen <nenfl24@gmail.com>
2019-04-25 21:31:24 -0500

    updeate

--- subnet.tf
+++ subnet.tf
@@ -6,14 +6,7 @@ resource "aws_subnet" "pub_1_subnet_eu_west_1a" {
     tags = {
         Name = "pub_test_1"    
     }
-resource "aws_subnet" "pub_2_subnet_eu_west_1a" {
-    vpc_id = "${aws_vpc.vpc_test.id}"
-    cidr_block = "${var.pub_2_subnet_cidr}"
-    map_public_ip_on_launch = true  #any instance created in this subnet will have a public IP
-    availability_zone = "eu-west-1a"
-    tags = {
-        Name = "pub_test_2"    
-    }
+
   
 }
 resource "aws_subnet" "priv_1_subnet_eu_west_1a" {
@@ -22,13 +15,6 @@ resource "aws_subnet" "priv_1_subnet_eu_west_1a" {
     availability_zone = "eu-west-1a"
     tags = {
         Name = "priv_test_1"    
-    }
-resource "aws_subnet" "priv_2_subnet_eu_west_1a" {
-    vpc_id = "${aws_vpc.vpc_test.id}"
-    cidr_block = "${var.priv_2_subnet_cidr}"
-    availability_zone = "eu-west-1a"
-    tags = {
-        Name = "priv_test_2"    
     }
   
 }
