4aa709dee013cdea363972ba1217839c842afce7
florinen <florinen>
2019-04-26 16:20:59 -0500

    added second subnet

--- subnet.tf
+++ subnet.tf
@@ -7,16 +7,16 @@ resource "aws_subnet" "pub_1_subnet_eu_west_1a" {
         Name = "pub_test_1"    
     }
 }
-#resource "aws_subnet" "pub_2_subnet_eu_west_1b" {
- #   vpc_id = "${aws_vpc.vpc_test.id}"
- #   cidr_block = "${var.pub_2_subnet_cidr}"
- #   map_public_ip_on_launch = false
- #   availability_zone = "eu-west-1b"
- #   tags = {
- #       Name = "pub_test_2"
-  #  }
+resource "aws_subnet" "pub_2_subnet_eu_west_1b" {
+    vpc_id = "${aws_vpc.vpc_test.id}"
+    cidr_block = "${var.pub_2_subnet_cidr}"
+    map_public_ip_on_launch = false
+    availability_zone = "eu-west-1b"
+    tags = {
+        Name = "pub_test_2"
+    }
   
-#}
+}
 resource "aws_subnet" "priv_1_subnet_eu_west_1a" {
     vpc_id = "${aws_vpc.vpc_test.id}"
     cidr_block = "${var.priv_1_subnet_cidr}"
