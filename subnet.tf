291acc8515d30a621496a7c0e945ac4b439c0c1f
florinen <nenfl24@gmail.com>
2019-04-26 13:09:43 -0500

    changed maped ip to false

--- subnet.tf
+++ subnet.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "pub_1_subnet_eu_west_1a" {
     vpc_id = "${aws_vpc.vpc_test.id}"
     cidr_block = "${var.pub_1_subnet_cidr}"
-    map_public_ip_on_launch = true  #any instance created in this subnet will have a public IP
+    map_public_ip_on_launch = false  #any instance created in this subnet will have a public IP
     availability_zone = "eu-west-1a"
     tags = {
         Name = "pub_test_1"    
@@ -9,7 +9,7 @@ resource "aws_subnet" "pub_1_subnet_eu_west_1a" {
 resource "aws_subnet" "pub_2_subnet_eu_west_1b" {
     vpc_id = "${aws_vpc.vpc_test.id}"
     cidr_block = "${var.pub_2_subnet_cidr}"
-    map_public_ip_on_launch = true
+    map_public_ip_on_launch = false
     availability_zone = "eu-west-1b"
     tags = {
         Name = "pub_test_2"
