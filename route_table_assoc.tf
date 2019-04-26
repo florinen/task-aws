41581b8b2d5ffa7ed465b8690ad0ac39904efc92
florinen <nenfl24@gmail.com>
2019-04-26 10:20:13 -0500

    reverted back to one subnet

--- route_table_assoc.tf
+++ route_table_assoc.tf
@@ -1,25 +1,14 @@
-# Associate subnet pub_1_subnet_eu_west_1a to public route table
+# Associate subnet pub_2_subnet_eu_west_1a to public route table
 resource "aws_route_table_association" "pub_1_subnet_eu_west_1a_assoc" {
     subnet_id = "${aws_subnet.pub_1_subnet_eu_west_1a.id}"
     route_table_id = "${aws_vpc.vpc_test.main_route_table_id}"
   
 }
-# Associate subnet pub_2_subnet_eu_west_1b to public route table
-#resource "aws_route_table_association" "pub_2_subnet_eu_west_1b_assoc" {
- #   subnet_id = "${aws.pub_2_subnet_eu_west_1b.id}"
-#    route_table_id = "${aws_vpc.vpc_test.main_route_table_id}"
-  
-#}
 # Associate subnet priv_1_subnet_eu_west_1a to private route table
 resource "aws_route_table_association" "priv_1_subnet_eu_west_1a_assoc" {
-    subnet_id = "${aws_subnet.priv_1_subnet_eu_west_1a.subnet_id}"
+    subnet_id = "${aws_subnet.priv_1_subnet_eu_west_1a.id}"
     route_table_id = "${aws_route_table.priv_route_table.id}"
   
 }
-# Associate subnet priv_2_subnet_eu_west_1b to private route table
-#resource "aws_route_table_association" "priv_2_subnet_eu_west_1b_assoc" {
- #   subnet_id = "${aws_subnet.priv_2_subnet_eu_west_1b.id}"
-  #  route_table_id = "${aws_route_table.priv_route_table.id}"
-  
-#}
+
 
