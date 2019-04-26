368dada8990c7bb1b6de7605b72c7868240e13b6
florinen <nenfl24@gmail.com>
2019-04-25 20:52:40 -0500

    added square brackets

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
     ami = "${lookup(var.amis, var.aws_region)}"
     count = "${var.count}"
     key_name = "${var.key_name}"
-    vpc_security_group_ids = "${aws_security_group.web.id}"
+    vpc_security_group_ids = ["${aws_security_group.web.id}"]
     instance_type = "${var.instance_type}"
     associate_public_ip_address = "true" # It will asign a public ip if set true
 
