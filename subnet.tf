2c022607bf32f7aa522df1d0e9ebaeaaa5b650f8
florinen <nenfl24@gmail.com>
2019-04-25 21:33:50 -0500

    updated file

--- subnet.tf
+++ subnet.tf
@@ -6,7 +6,14 @@ resource "aws_subnet" "pub_1_subnet_eu_west_1a" {
     tags = {
         Name = "pub_test_1"    
     }
-
+resource "aws_subnet" "pub_2_subnet_eu_west_1b" {
+    vpc_id = "${aws_vpc.vpc_test.id}"
+    cidr_block = "${var.pub_1_subnet_cidr}"
+    map_public_ip_on_launch = true  #any instance created in this subnet will have a public IP
+    availability_zone = "eu-west-1b"
+    tags = {
+        Name = "pub_test_2"    
+    }
   
 }
 resource "aws_subnet" "priv_1_subnet_eu_west_1a" {
