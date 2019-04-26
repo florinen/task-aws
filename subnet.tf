9407ac5d404af31dd9e3cf62d34071436b8329b2
florinen <florinen>
2019-04-26 15:01:17 -0500

    deleted curly braket

--- subnet.tf
+++ subnet.tf
@@ -6,6 +6,7 @@ resource "aws_subnet" "pub_1_subnet_eu_west_1a" {
     tags = {
         Name = "pub_test_1"    
     }
+}
 resource "aws_subnet" "pub_2_subnet_eu_west_1b" {
     vpc_id = "${aws_vpc.vpc_test.id}"
     cidr_block = "${var.pub_2_subnet_cidr}"
@@ -16,8 +17,6 @@ resource "aws_subnet" "pub_2_subnet_eu_west_1b" {
     }
   
 }
-
-  }
 resource "aws_subnet" "priv_1_subnet_eu_west_1a" {
     vpc_id = "${aws_vpc.vpc_test.id}"
     cidr_block = "${var.priv_1_subnet_cidr}"
