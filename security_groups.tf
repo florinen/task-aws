77eb5c3cac7efe20e255e648db9203d7a9fca17b
florinen <nenfl24@gmail.com>
2019-04-25 19:05:09 -0500

    square brackets added

--- security_groups.tf
+++ security_groups.tf
@@ -7,25 +7,25 @@ resource "aws_security_group" "web" {
         from_port = "80"
         to_port = "80"
         protocol = "tcp"
-        cidr_blocks = "[${var.from_anywhere}]"
+        cidr_blocks = ["${var.from_anywhere}"]
     }
     ingress = {
         from_port = "443"
         to_port = "443"
         protocol = "tcp"
-        cidr_blocks = "[${var.from_anywhere}]"
+        cidr_blocks = ["${var.from_anywhere}"]
     }
     egress = {
         from_port = "1433"
         to_port   = "1433"
         protocol  = "tcp"
-        cidr_blocks = "[${var.priv_1_subnet_cidr}]"
+        cidr_blocks = ["${var.priv_1_subnet_cidr}"]
     }
     egress = {
         from_port = "3306"
         to_port   = "3306"
         protocol  = "tcp"
-        cidr_blocks = "[${var.priv_1_subnet_cidr}]"
+        cidr_blocks = ["${var.priv_1_subnet_cidr}"]
     }
     vpc_id = "${aws_vpc.vpc_test.vpc_id}"
 
