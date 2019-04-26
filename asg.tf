66427e32614367298b925adabb5ba11c515edaef
florinen <florinen>
2019-04-26 16:17:20 -0500

    removed az

--- asg.tf
+++ asg.tf
@@ -11,7 +11,7 @@ resource "aws_autoscaling_group" "web_asg" {
   launch_configuration = "${aws_launch_configuration.web_launch-config.name}"
   min_size = "2"
   max_size = "4"
-  availability_zones = ["eu-west-1b","eu-west-1a","eu-west-1c"]
+  availability_zones = ["eu-west-1a"]
   vpc_zone_identifier = ["${aws_subnet.priv_1_subnet_eu_west_1a.id}"]#, "${aws_subnet.dev2.id}"]
   
 }
