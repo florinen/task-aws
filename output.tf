27c41a1de4b24fc540099104238a9fa446661c71
florinen <nenfl24@gmail.com>
2019-04-27 01:16:00 -0500

    output file created

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
-output "instance_ip" {
-  value = ["${aws_instance.web.*.public_ip}"]
+output "instance_id" {
+  value = ["${aws_launch_configuration.web_launch-config.*.ip}"]
 }
 output "elb_dns_name" {
   value = "${aws_elb.elb_web.elb_dns_name}"
