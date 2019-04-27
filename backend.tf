5ebde37effb91ac19e8dc80227d1215b939ae925
florinen <florinen>
2019-04-27 12:46:44 -0500

    create bucket

--- backend.tf
+++ backend.tf
@@ -1,19 +1,27 @@
-terraform {
- backend "s3" {
-   bucket = "task-florin"
-   key = "test"
-   acl    = "log-delivery-write"
-   region = "eu-west-1"
+#terraform {
+ #backend "s3" {
+  ##key = "test"
+   #acl    = "log-delivery-write"
+  # region = "eu-west-1"
  
-  }
-resource "aws_s3_bucket" "versioning_bucket" {
-  bucket = "task-florin"
-  acl    = "private"
+  #}
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "my-lb-web-log-bucket"
+  acl    = "log-delivery-write"
+  region = "eu-west-1"
 
-  versioning {
-    enabled = true
-  }
- 
  }
+
+resource "aws_s3_bucket" "tf_bucker" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+  region = "eu-west-1"
+  
+  logging {
+    target_bucket = "${aws_s3_bucket.log_bucket.id}"
+    target_prefix = "log/"
+  }
+
+  }
 
 }
