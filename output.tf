d47a5ddab533b5fa3aab34bb1059adda4e9adbb3
florinen <florinen>
2019-04-27 14:28:57 -0500

    added extra outp ut

--- output.tf
+++ output.tf
@@ -5,3 +5,9 @@ output "lb_dns_name" {
   value = "${aws_lb.lb_web.dns_name}"
 }
 
+output "VPC" {
+  value = "${aws_vpc.vpc_test.id}"
+}
+output "SUBNET" {
+  value = "${aws_subnet.priv_1_subnet_eu_west_1a.id}"
+}
