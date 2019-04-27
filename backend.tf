6b8708f3bd9839a641ff09e57ece552bc2e6715a
florinen <florinen>
2019-04-27 12:12:24 -0500

    added versioning

--- backend.tf
+++ backend.tf
@@ -4,10 +4,16 @@ terraform {
    key = "test"
    acl    = "log-delivery-write"
    region = "eu-west-1"
- versioning {
+ 
+  }
+resource "aws_s3_bucket" "versioning_bucket" {
+  bucket = "task-florin"
+  acl    = "private"
+
+  versioning {
     enabled = true
   }
-
+ 
  }
 
 }
