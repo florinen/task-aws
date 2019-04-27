94cdfcaeb78fb5ef847e5b1edc2f5a6361759b7d
florinen <florinen>
2019-04-27 13:47:18 -0500

    updat ed file

--- backend.tf
+++ backend.tf
@@ -4,9 +4,7 @@ terraform {
     key = "test"
     acl    = "log-delivery-write"
     region = "eu-west-1"
-  versioning {
-    enabled = true
-  }
+  
   logging {
     target_bucket = "task-florin"
     target_prefix = "log/"
