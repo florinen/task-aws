28d3a591385c5a1e40b009218f098bf60d028712
florinen <florinen>
2019-04-27 10:53:17 -0500

    fixed  lb dns name

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "ip" {
   value = ["${aws_eip.test_eip.*.public_ip}"]
 }
-output "elb_dns_name" {
-  value = "${aws_elb.elb_web.dns_name}"
+output "lb_dns_name" {
+  value = "${aws_lb.lb_web.dns_name}"
 }
 
