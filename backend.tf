6adc8448bb7e5cfe7299b92bba5f64f8d8118df5
florinen <florinen>
2019-04-27 13:40:36 -0500

    backet reconfigured

--- backend.tf
+++ backend.tf
@@ -1,29 +1,21 @@
-#terraform {
- #backend "s3" {
-  ##key = "test"
-   #acl    = "log-delivery-write"
-  # region = "eu-west-1"
- 
-  #}
-resource "aws_s3_bucket" "log_bucket" {
-  bucket = "my-lb-web-log-bucket"
+terraform {
+ backend "s3" {
+  key = "test"
   acl    = "log-delivery-write"
   region = "eu-west-1"
-  versioning {
+ versioning {
     enabled = true
   }
-
- }
-
-resource "aws_s3_bucket" "tf_bucket" {
-  bucket = "my-tf-test-bucket"
-  acl    = "private"
-  region = "eu-west-1"
-  
-  logging {
-    target_bucket = "${aws_s3_bucket.log_bucket.id}"
+ logging {
+    target_bucket = "task-florin"
     target_prefix = "log/"
   }
+ 
+ }
+}
 
-  }
+
+  
+
+
 
