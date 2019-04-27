07f8c79ece6188a58c9fa91a1e09511292c89a63
florinen <nenfl24@gmail.com>
2019-04-27 01:17:12 -0500

    fixed elb dns

--- output.tf
+++ output.tf
@@ -2,6 +2,6 @@ output "instance_id" {
   value = ["${aws_launch_configuration.web_launch-config.*.ip}"]
 }
 output "elb_dns_name" {
-  value = "${aws_elb.elb_web.elb_dns_name}"
+  value = "${aws_elb.elb_web.dns_name}"
 }
 
