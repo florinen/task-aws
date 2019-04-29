9fe38fded359c7bb455b577a65b785c6669289c5
florinen <florinen>
2019-04-29 10:49:57 -0500

    updated the capacity

--- asg.tf
+++ asg.tf
@@ -19,10 +19,10 @@
 resource "aws_autoscaling_group" "web_asg" {
   name                      = "web_asg_test"
   max_size                  = 5
-  min_size                  = 2
+  min_size                  = 1
   health_check_grace_period = 300
   health_check_type         = "ELB"
-  desired_capacity          = 4
+  desired_capacity          = 2
   force_delete              = true
   launch_configuration      = "${aws_launch_configuration.l-config.name}"
   vpc_zone_identifier       = ["${aws_subnet.pub_1_subnet_eu_west_1a.id}", "${aws_subnet.pub_2_subnet_eu_west_1b.id}"]
