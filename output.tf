beeaeb9b5ca00162660314c50ef57cbb3c014c02
florinen <florinen>
2019-05-02 12:26:02 -0500

    test

--- output.tf
+++ output.tf
@@ -6,11 +6,11 @@ output "VPC" {
   description = "VPC created"
   value = "${aws_vpc.vpc_test.id}"
 }
-output "SUBNET" {
+output "SUBNET_10" {
   description = "Private subnet 10"
   value = "${aws_subnet.priv_1_subnet_eu_west_1a.id}"
 }
-output "SUBNET" {
+output "SUBNET_11" {
   description = "Private subnet 11"
   value = "${aws_subnet.priv_2_subnet_eu_west_1b.id}"
 }
