9508d828f8f23beeddc30c9c917a47a032d34e9f
florinen <florinen>
2019-05-02 12:04:32 -0500

    added instance IP

--- output.tf
+++ output.tf
@@ -1,5 +1,6 @@
-output "ip" {
-  value = ["${aws_eip.test_eip.*.public_ip}"]
+output "public_ip" {
+  description = "Public IP of instance"
+  value = ["${aws_launch_configuration.l-config.*.public_ip}"]
 }
 output "lb_dns_name" {
   value = "${aws_lb.lb_web.dns_name}"
