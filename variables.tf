031a985b64295bd99512fbedd8f464b366c3137e
florinen <florinen>
2019-05-02 11:34:43 -0500

    updated ami

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "amis" {
     description = "AMIs by region"
-    default = "ami-0aa438a3e23a5036d" # Centos 7 x64
+    default = "ami-06f4fc0f906ec9ccf" # Custom Centos 7 x64
 }
 
 variable "vpc_cidr" {
