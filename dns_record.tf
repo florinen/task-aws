0f23157a9934e5dcf1161b6c8d10b975bb720dc9
florinen <florinen>
2019-05-02 17:28:32 -0500

    update

--- dns_record.tf
+++ dns_record.tf
@@ -20,7 +20,7 @@ resource "aws_route53_record" "nextcloud" {
   type    = "A"
 
   alias {
-    name                   = "${aws_elb.lb_web.name}"
+    name                   = "${aws_elb.lb_web.lb_web_dns}"
     zone_id                = "${aws_elb.devopnet.zone_id}"
     evaluate_target_health = true
   }
