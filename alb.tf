959af9f5307209befa0b4848bae3eb3418fe4c27
florinen <florinen>
2019-05-02 09:01:48 -0500

    fixet port

--- alb.tf
+++ alb.tf
@@ -39,9 +39,6 @@ resource "aws_lb_listener" "front_end" {
   load_balancer_arn = "${aws_lb.lb_web.arn}"
   port              = "80"
   protocol          = "HTTP"
-  port = "3000"
-  protocol = "HTTP"
-  
   default_action {
     type             = "forward"
     target_group_arn = "${aws_lb_target_group.web_tg.arn}"
