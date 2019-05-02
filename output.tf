e52bea78a565e047074c98c23765e150c928d862
florinen <florinen>
2019-05-02 12:24:15 -0500

    added output for subnets and descriptions

--- output.tf
+++ output.tf
@@ -1,14 +1,28 @@
 output "public_ip" {
-  description = "Public IP of instance"
-  value = ["${aws_launch_configuration.l-config.*.IP}"]
+  description = "Public IP of eip"
+  value = ["${aws_eip.aws_eip.*.public_ip}"]
 }
-output "lb_dns_name" {
-  value = "${aws_lb.lb_web.dns_name}"
-}
-
 output "VPC" {
+  description = "VPC created"
   value = "${aws_vpc.vpc_test.id}"
 }
 output "SUBNET" {
+  description = "Private subnet 10"
   value = "${aws_subnet.priv_1_subnet_eu_west_1a.id}"
+}
+output "SUBNET" {
+  description = "Private subnet 11"
+  value = "${aws_subnet.priv_2_subnet_eu_west_1b.id}"
+}
+output "SUBNET" {
+  description = "Public subnet 1"
+  value = "${aws_subnet.pub_1_subnet_eu_west_1a.id}"
+}
+output "SUBNET" {
+  description = "Public subnet 2"
+  value = "${aws_subnet.pub_2_subnet_eu_west_1b.id}"
+}
+output "lb_dns_name" {
+  description = "ALB DNS record"
+  value = "${aws_lb.lb_web.dns_name}"
 }
