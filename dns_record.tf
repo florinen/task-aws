112ebb0d60666ee31e3da2f4b1d88758ecd1b38a
florinen <florinen>
2019-05-02 19:20:23 -0500

    added  evaluate helth target

--- dns_record.tf
+++ dns_record.tf
@@ -25,6 +25,7 @@ resource "aws_route53_record" "nextcloud" {
   alias {
       name = "${aws_lb.lb_web.dns_name}"
       zone_id = "${aws_route53_zone.devopnet.id}"
+      evaluate_target_health = true
   }
 }
 
