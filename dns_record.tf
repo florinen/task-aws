55cb0aba1fbd8c62700e56f6c226bae1a280c956
florinen <florinen>
2019-05-03 18:56:19 -0500

    added target dns and zone id for alias

--- dns_record.tf
+++ dns_record.tf
@@ -38,7 +38,9 @@ resource "aws_route53_record" "nextcloud" {
   #ttl     = "300"
   #records = ["10.0.0.1"]
   alias {
-      name = "${aws_lb.lb_web.zone_id}"
+      target_dns_name = "${aws_lb.lb_web.dns_name}"
+      target_zone_id = "${aws_lb.lb_web.zone_id}"
+      #name = "${aws_lb.lb_web.zone_id}"
       zone_id = "${aws_route53_zone.devopnet.zone_id}"
       evaluate_target_health = true
       
