460046abdd3b45d3f14ecab01c3807aaf0dc2939
florinen <florinen>
2019-05-03 20:29:25 -0500

    added var for name record

--- dns_record.tf
+++ dns_record.tf
@@ -22,7 +22,7 @@ resource "aws_route53_record" "nextcloud" {
 }
 resource "aws_route53_record" "test" {
   zone_id = "${aws_route53_zone.devopnet.zone_id}"
-  name = "test.${aws_route53_zone.devopnet.name}"
+  name = "${var.name_record}.${aws_route53_zone.devopnet.name}"
   type = "A"
   ttl = "300"
   records = ["${var.ip_add}"]
