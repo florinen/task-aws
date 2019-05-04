a2c081bb07ded12ccc489a3edd319e72cd95ce1a
florinen <florinen>
2019-05-03 19:04:36 -0500

    update

--- dns_record.tf
+++ dns_record.tf
@@ -29,7 +29,7 @@ resource "aws_route53_zone" "devopnet" {
   name = "${var.parent_zone_name}"
   
 }
-#data "aws_elb_hosted_zone_id" "dns_name" {}
+data "aws_elb_hosted_zone_id" "current" {}
 
 resource "aws_route53_record" "nextcloud" {
   zone_id = "${aws_route53_zone.devopnet.id}"
@@ -38,9 +38,9 @@ resource "aws_route53_record" "nextcloud" {
   #ttl     = "300"
   #records = ["10.0.0.1"]
   alias {
-      target_dns_name = "${aws_lb.lb_web.dns_name}"
+      #target_dns_name = "${aws_lb.lb_web.dns_name}"
       #target_zone_id = "${aws_lb.lb_web.zone_id}"
-      name = "${aws_lb.lb_web.zone_id}"
+      name = "${data.aws_elb_hosted_zone_id.current.hosted_zone_id}"
       zone_id = "${aws_route53_zone.devopnet.zone_id}"
       evaluate_target_health = true
       
