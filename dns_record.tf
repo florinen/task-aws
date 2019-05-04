1f25185358a090824432cff25ead1bb366fe1e15
florinen <florinen>
2019-05-03 19:40:54 -0500

    1

--- dns_record.tf
+++ dns_record.tf
@@ -38,14 +38,12 @@ resource "aws_route53_record" "nextcloud" {
   #ttl     = "300"
   #records = ["10.0.0.1"]
   alias {
-      name = "${aws_lb.lb_web.dns_name}"
-      #target_dns_name = "${aws_lb.lb_web.dns_name}"
+      target_dns_name = "${aws_elb_hosted_zone_id.id}"
       #target_zone_id = "${aws_lb.lb_web.zone_id}"
       name = "${data.aws_elb_hosted_zone_id.current.id}"
       zone_id = "${aws_route53_zone.devopnet.zone_id}"
       evaluate_target_health = false
-      
-      
+      t
   }
 }
 
