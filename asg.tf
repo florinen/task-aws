0ae9a672dab14cfb66424063de58e06319648997
florinen <florinen>
2019-05-04 20:18:29 -0500

    changed health chacks type

--- asg.tf
+++ asg.tf
@@ -5,7 +5,7 @@ resource "aws_autoscaling_group" "web_asg" {
   max_size                  = 5
   min_size                  = 1
   health_check_grace_period = 300
-  health_check_type         = "ELB"
+  health_check_type         = "EC2"
   desired_capacity          = 1
   force_delete              = true
   launch_configuration      = "${aws_launch_configuration.l-config.name}"
