dcbc4f978433eea9bbb2ce664f839e429ae87ef8
florinen <florinen>
2019-05-08 16:46:07 -0500

    update file

--- image/.print.sh
+++ image/.print.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # run packer (prints to stdout, but stores the output in a variable)
-sudo packer_out=$(packer build golden_image.json |  tee /dev/tty)
+packer_out=$(packer build golden_image.json |  tee output.txt)
 
 # packer prints the id of the generated AMI in its last line
-sudo ami=$(echo "$packer_out" | tail -c 30 | perl -n -e'/: (ami-.+)$/ && print $1')
+ami=$(echo "$packer_out" | tail -c 30 | perl -n -e'/: (ami-.+)$/ && print $1')
 
 # create the 'ami.tf' file from the template:
 sudo export AMI_GENERATED_BY_PACKER="$ami" && envsubst < ami.tf.template > /root/task-aws/ami.tf
