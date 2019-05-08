3ca7028b731668aaa05022c40e1c92a604d0346a
florinen <florinen>
2019-05-08 16:44:11 -0500

    packer output new file

--- image/packer_output.sh
+++ image/packer_output.sh
@@ -3,11 +3,11 @@
 set -e
 
 packer build \
-    bastion.json | tee packer_output.txt
+    golden_image.json | tee packer_output.txt
 
 cat packer_output.txt | tail -n 2 \ 
   | sed '$ d' \
-  | sed "s/eu-west-1: /packer_built_bastion_ami = \"/" \
+  | sed "s/eu-west-1: /packer_build_ami = \"/" \
   | sed -e 's/[[:space:]]*$/\"/' > packer_ami.tfvars
 
 cat packer_ami.tfvars
