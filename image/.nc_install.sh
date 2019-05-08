39e36ce438c08054b215af2d767d92a54eb5a769
florinen <florinen>
2019-05-08 12:56:57 -0500

    removed  packer output

--- image/.nc_install.sh
+++ image/.nc_install.sh
@@ -76,13 +76,5 @@ sudo semanage fcontext -a -t httpd_sys_rw_content_t '/var/www/html/nextcloud/.us
 
 sudo restorecon -Rv '/var/www/html/nextcloud/'
 
-#sudo echo "Install finished"
+sudo echo "Install finished"
 
-# run packer (prints to stdout, but stores the output in a variable)
-packer_out=$(packer golden_image.json | tee /dev/tty)
-
-# packer prints the id of the generated AMI in its last line
-ami=$(echo "$packer_out" | tail -c 30 | perl -n -e'/: (ami-.+)$/ && print $1')
-
-# create the 'ami.tf' file from the template:
-export AMI_GENERATED_BY_PACKER="$ami" && envsubst < ami.tf.template > /root/task-aws/ami.tf
