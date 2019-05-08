e73ada64d362b57f723200d061d8362f31510f41
florinen <florinen>
2019-05-08 17:07:27 -0500

    added sudo

--- image/packer_output.sh
+++ image/packer_output.sh
@@ -5,9 +5,9 @@ set -e
 packer build \
     golden_image.json | tee packer_output.txt
 
-cat packer_output.txt | tail -n 2 \ 
+sudo cat packer_output.txt | tail -n 2 \ 
   | sed '$ d' \
   | sed "s/eu-west-1: /packer_build_ami = \"/" \
   | sed -e 's/[[:space:]]*$/\"/' > packer_ami.tfvars
 
-cat packer_ami.tfvars
+sudo cat packer_ami.tfvars
