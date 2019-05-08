a4b9015d11e8dd942f30c6a8efc875decb428bfd
florinen <florinen>
2019-05-08 16:17:56 -0500

    added sudo for cmd

--- image/.print.sh
+++ image/.print.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # run packer (prints to stdout, but stores the output in a variable)
-packer_out=$(packer build golden_image.json | tee /dev/tty)
+sudo packer_out=$(packer build golden_image.json | sudo tee /dev/tty)
 
 # packer prints the id of the generated AMI in its last line
-ami=$(echo "$packer_out" | tail -c 30 | perl -n -e'/: (ami-.+)$/ && print $1')
+sudo ami=$(echo "$packer_out" | tail -c 30 | perl -n -e'/: (ami-.+)$/ && print $1')
 
 # create the 'ami.tf' file from the template:
-export AMI_GENERATED_BY_PACKER="$ami" && envsubst < ami.tf.template > /root/task-aws/ami.tf
+sudo export AMI_GENERATED_BY_PACKER="$ami" && envsubst < ami.tf.template > /root/task-aws/ami.tf
